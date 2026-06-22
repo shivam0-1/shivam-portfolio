81b2339391e271a0c910c7df43e1687ef8cbd928
Shivam Kumar <shivam2593479@gmail.com>
2026-06-22 15:32:12 +0530

    feat: add tailwind v4, framer-motion, aos, design tokens

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
-import { StrictMode } from 'react'
+import { StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import './index.css'
 import App from './App.jsx'
+
+AOS.init({
+  duration: 800,
+  once: true,
+  easing: 'ease-out-cubic',
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
